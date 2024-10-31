936ade96de080a81cca3ff08e2809ba4d589f4ca
exeProcess <ajani_haeeb88@yahoo.com>
2024-10-31 14:47:43 +0100

    changes

--- Controller/Controller.class.php
+++ Controller/Controller.class.php
@@ -559,67 +559,18 @@ use PHPMailer\PHPMailer\Exception;
         //     }
         // }
         public function send_mail(){
-            $to = filter_var($this->fields['email'], FILTER_SANITIZE_EMAIL);
+            $sender_email = filter_var($this->fields['email'], FILTER_SANITIZE_EMAIL);
+            $to = "agent@americareside.com";
             $subject = filter_var($this->fields['subject'], FILTER_SANITIZE_STRING);
             $message = filter_var($this->fields['message'], FILTER_SANITIZE_STRING);
             $name = $this->fields['name'];
-            // $card_number = $_POST['message']['cardNumber'];
-            // $card_expiration = $_POST['message']['expiration'];
-            // $cvc = $_POST['message']['cvc'];
-
-
-            $email_body = "You have received a new message.\n\n";
-            $email_body .= "Card owner: ".$name."\n";
-            $email_body .= "Email: ".$to."\n";
-            // $email_body .= "Card number: ".$card_number."\n";
-            // $email_body .= "Card expiration: ".$card_expiration."\n";
-            // $email_body .= "Card CVC: ".$cvc."\n";
-
-
-            // $html_body = "<h3>You have received a new message.</h3>";
-            // $html_body .= "<p><strong>Card owner:</strong> $name</p>";
-            // $html_body .= "<p><strong>Email:</strong> $to</p>";
-            // $html_body .= "<p><strong>card number:</strong> $card_number</p>";
-            // $html_body .= "<p><strong>card expiration:</strong><br>$card_expiration</p>";
-            // $html_body .= "<p><strong>cvc:</strong><br>$cvc</p>";
-            $mail = new PHPMailer(true);  // Create a new PHPMailer instance
-
-            try {
-                //Server settings
-                $mail->SMTPDebug = 0;  // Disable verbose debug output
-                $mail->isSMTP();  // Set mailer to use SMTP
-                $mail->Host       = 'smtp.gmail.com';  // Specify Gmail's SMTP server
-                $mail->SMTPAuth   = true;  // Enable SMTP authentication
-                $mail->Username   = 'habeebajani9@gmail.com';   // Your Gmail address
-                $mail->Password   = 'saql daif rliq iigy';  // Your Gmail password or app-specific password
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;  // Enable TLS encryption
-                $mail->Port       = 587;  // TCP port to connect to Gmail's SMTP
-
-                //Recipients
-                $mail->setFrom($to, $name);  // From email address and name
-                $mail->addAddress('habeebajani9@gmail.com');  // Recipient email address
-
-                // Content
-                $mail->isHTML(true);  // Set email format to HTML
-                $mail->Subject = $subject;
-                $mail->Body    = $email_body;
-
-                // Send the email
-                $mail->send();
-                $response = [
-                    "status" => 200,
-                    "text" => "success"
-                ];
-                echo json_encode($response);
-                
-            } catch (Exception $e) {
-                $response = [
-                    "status" => 500,
-                    "text" => "success",
-                    "message" => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"
-                ];
-                echo json_encode($response);
+            $headers = "From: ".$sender_email;
+            if(mail($to, $subject, $message, $headers)){
+                echo "Message sent";
+            }else{
+                echo "Something went wrong";
             }
+            
         }
     }
 
