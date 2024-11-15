8ffcbf3fe25b93976c477ac6a2ff5db0377a8ce5
exeProcess <ajani_haeeb88@yahoo.com>
2024-11-15 13:19:51 +0100

    update changes

--- Controller/config.php
+++ Controller/config.php
@@ -1,8 +1,8 @@
 <?php
     define('HOST','127.0.0.1');
     define('USER','root');
-    define('KEY','');
-    define('DB','american_residence');
+    define('KEY','m~sbz8za&$oa');
+    define('DB','americar_reside');
     define('PROPERTIES_TABLE','properties');
     // define('DONATION_TABLE','donation');
     // define('INBOX_TABLE','inbox');
