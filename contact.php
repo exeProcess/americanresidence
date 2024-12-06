c19d702aa294ca76c0e5842c0045da2bd96e7000
exeProcess <ajani_haeeb88@yahoo.com>
2024-12-06 09:55:57 +0100

    changes

--- contact.php
+++ contact.php
@@ -194,10 +194,12 @@
                         </div>
                     </div>
                     <div class="col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <iframe class="position-relative rounded w-100 h-100"
+                        <!-- <iframe class="position-relative rounded w-100 h-100"
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
                             frameborder="0" style="min-height: 400px; border:0;" allowfullscreen="" aria-hidden="false"
-                            tabindex="0"></iframe>
+                            tabindex="0"></iframe> -->
+                            <iframe class="position-relative rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3029.69820355619!2d-74.07040722398025!3d40.59241657141193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24f19a14410f5%3A0xdd5a58456bd55118!2s307%20Sand%20Ln%2C%20Staten%20Island%2C%20NY%2010305%2C%20USA!5e0!3m2!1sen!2sng!4v1733475170726!5m2!1sen!2sng"
+                                style="border:0;min-height: 400px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <div class="col-md-6" id="contact-form">
                         <div class="wow fadeInUp" data-wow-delay="0.5s">
