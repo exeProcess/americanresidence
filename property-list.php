c9072ce9dd83f96474a519cbc92984482ee30ef2
exeProcess <ajani_haeeb88@yahoo.com>
2024-12-06 09:12:44 +0100

    changes

--- property-list.php
+++ property-list.php
@@ -622,9 +622,9 @@
                 <div class="row g-5">
                     <div class="col-lg-3 col-md-6">
                         <h5 class="text-white mb-4">Get In Touch</h5>
-                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                        <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>307 Sand Ln, Staten Island, NY 10305, United States</p>
+                        <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+1 (786) 307 1343</p>
+                        <p class="mb-2"><i class="fa fa-envelope me-3"></i>americar@americareside.com</p>
                         <div class="d-flex pt-2">
                             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                             <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -665,10 +665,10 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <h5 class="text-white mb-4">Newsletter</h5>
-                        <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                        <p>Subscribe to our newsletter</p>
                         <div class="position-relative mx-auto" style="max-width: 400px;">
                             <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Subscribe</button>
                         </div>
                     </div>
                 </div>
@@ -677,17 +677,16 @@
                 <div class="copyright">
                     <div class="row">
                         <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                            &copy; <a class="border-bottom" href="#">American Residence</a>, All Right Reserved. 
-							
+                            &copy; <a class="border-bottom" href="#">America reside</a>
                         </div>
-                        <!-- <div class="col-md-6 text-center text-md-end">
+                        <div class="col-md-6 text-center text-md-end">
                             <div class="footer-menu">
                                 <a href="">Home</a>
                                 <a href="">Cookies</a>
                                 <a href="">Help</a>
                                 <a href="">FQAs</a>
                             </div>
-                        </div> -->
+                        </div>
                     </div>
                 </div>
             </div>
